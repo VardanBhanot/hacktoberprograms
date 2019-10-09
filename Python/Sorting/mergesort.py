3cf75fe2406c4f16b23c55d0976957efbe22fbc1
Sriram2001 <sriramudupi19@gmail.com>
2019-10-10 01:34:31 +0530

    Added merge sort in python

---
+++ Python/Sorting/mergesort.py
@@ -0,0 +1,41 @@
+def mergeSort(arr):
+    if(len(arr)>1):
+        mid = len(arr)//2
+        leftArray = arr[:mid]
+        rightArray = arr[mid:]
+        mergeSort(leftArray)
+        mergeSort(rightArray)
+        i=j=k=0
+        while i<len(leftArray) and j<len(rightArray):
+            if leftArray[i] < rightArray[j]:
+                arr[k] = leftArray[i]
+                i += 1
+            else :
+				arr[k] = rightArray[j] 
+				j += 1
+            k += 1
+        
+        while i< len(leftArray):
+            arr[k] = leftArray[i]
+            i += 1
+            k += 1
+        
+        while j< len(rightArray):
+            arr[k] = rightArray[j]
+            j += 1
+            k +=1
+
+# Code to print the list 
+def printList(arr): 
+	for i in range(len(arr)):		 
+		print(arr[i],end=" ") 
+	print() 
+
+# driver code 
+if __name__ == '__main__': 
+	arr = [12, 11, 13, 5, 6, 7] 
+	print ("Input array: ") 
+	printList(arr) 
+	mergeSort(arr) 
+	print("Sorted array is: ") 
+	printList(arr) 
