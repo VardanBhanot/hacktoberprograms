b598f76b79224804f5ba8f86deceb80f20424183
Bigyan Timislina <42671917+biggu12@users.noreply.github.com>
2019-10-12 00:37:38 +0530

    Create Deafult constructor.cpp

---
+++ C++/Deafult constructor.cpp
@@ -0,0 +1,19 @@
+//deafult constructor
+#include<iostream>
+using namespace std;
+class Bigyan
+{
+	public:
+		float l,b;
+	Bigyan();	
+}k;
+Bigyan::Bigyan()
+{
+	l=5;
+	b=4;
+}
+main()
+{
+
+cout<<"Area of rectangle: "<<k.b*k.l;
+}
